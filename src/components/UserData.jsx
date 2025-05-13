3b990b588089a165926bbf047127426afe1437f2
SaiManikya Mallikharjun Khandavilli <saimallikharjun81@gmail.com>
2025-05-13 19:35:39 +0530

    first commit

--- src/components/UserData.jsx
+++ src/components/UserData.jsx
@@ -8,11 +8,11 @@ function UserData({ users }) {
   return (
     <>
       <div className="w-full">
-        <div className="max-w-2xl mx-auto mt-10 border rounded-lg h-40">
+        <div className="max-w-2xl mx-auto mt-20 border rounded-lg h-45">
           {users.map((user) => (
             <div
               key={user.id}
-              className="flex items-center space-x-4 p-1"
+              className="flex items-center space-x-4 p-0.5"
             >
               <img
                 src={user.profile_image}
